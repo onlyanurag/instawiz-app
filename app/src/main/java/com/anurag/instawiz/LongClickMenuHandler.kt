d5acc7ffd62a9550c3047b651839a269dc3575c3
Anurag <maanshichandela2007@gmail.com>
2025-04-23 20:41:17 +0530

    Create LongClickMenuHandler.kt

---
+++ app/src/main/java/com/anurag/instawiz/LongClickMenuHandler.kt
@@ -0,0 +1,30 @@
+package com.instawiz.android
+
+import android.content.Context
+import android.view.View
+import com.instawiz.android.utils.DialogUtils
+import com.instagram.mainactivity.InstagramMainActivity
+
+class LongClickMenuHandler : View.OnLongClickListener {
+
+    private val mainActivity : InstagramMainActivity
+
+    constructor (instagramMainActivity: InstagramMainActivity) {
+        this.mainActivity = instagramMainActivity
+    }
+
+    companion object {
+        fun setLongClickMenuHandler(instagramMainActivity: InstagramMainActivity, view: View) {
+            view.setOnLongClickListener(LongClickMenuHandler(instagramMainActivity))
+        }
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        val ctx: Context = v!!.context
+        val instagramMainActivity: InstagramMainActivity = this.mainActivity
+        DialogUtils.showInstaMoonOptionsDialog(ctx, instagramMainActivity)
+        return true
+    }
+
+
+}
