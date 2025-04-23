cf2caf06000e859d3fa759ec4e3189c8e877331f
Anurag <maanshichandela2007@gmail.com>
2025-04-23 20:20:57 +0530

    Create DeveloperUtils.kt

---
+++ app/src/main/java/com/anurag/instawiz/utils/DeveloperUtils.kt
@@ -0,0 +1,15 @@
+package com.instawiz.android.utils
+
+import android.content.Context
+import com.instagram.debug.devoptions.api.DeveloperOptionsLauncher
+import com.instagram.mainactivity.InstagramMainActivity
+
+
+class DeveloperUtils {
+    companion object {
+        fun openDeveloperMode(ctx: Context, instagramMainActivity: InstagramMainActivity) {
+            DeveloperOptionsLauncher.INSTANCE.loadAndLaunchDeveloperOptions(ctx, instagramMainActivity, instagramMainActivity.A05)
+            //DeveloperOptionsLauncher.INSTANCE.loadAndLaunchDeveloperOptions(ctx, instagramMainActivity.BIK(), instagramMainActivity, instagramMainActivity.A05)
+        }
+    }
+}
